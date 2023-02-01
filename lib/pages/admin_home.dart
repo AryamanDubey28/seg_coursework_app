cf7ed2f24e44cd9ac796510dcea5064483e67bac
Ali Alkhars <a-khars@hotmail.com>
2023-02-01 16:48:18 +0000

    Create a side-menu for the Admin UI

--- lib/pages/admin_home.dart
+++ lib/pages/admin_home.dart
@@ -25,5 +25,74 @@ class AdminHome extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Home'),
         ),
+        drawer: const NavigationDrawer(),
+      );
+}
+
+// The side-menu of the admin's UI (should be moved to another file)
+class NavigationDrawer extends StatelessWidget {
+  const NavigationDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Drawer(
+        child: SingleChildScrollView(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            buildHeader(context),
+            buildMenuItems(context),
+          ],
+        )),
+      );
+
+  // The header of the side-menu
+  Widget buildHeader(BuildContext context) => Container(
+        padding: EdgeInsets.only(
+          top: MediaQuery.of(context).padding.top,
+        ),
+      );
+
+  // The items of the side-menu
+  Widget buildMenuItems(BuildContext context) => Container(
+        padding: const EdgeInsets.all(10),
+        child: Wrap(
+          children: [
+            ListTile(
+              leading: const Icon(Icons.photo_size_select_actual_outlined),
+              title: const Text('Card Boards'),
+              onTap: () =>
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) => const AdminHome(),
+              )),
+            ),
+            ListTile(
+                leading: const Icon(Icons.event),
+                title: const Text('Visual Timetable'),
+                onTap: () {}),
+            ListTile(
+              leading: const Icon(Icons.child_care),
+              title: const Text('Activate Child Mode'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.color_lens_outlined),
+              title: const Text('Edit App Colours'),
+              onTap: () {},
+            ),
+            const Divider(
+              color: Colors.black54,
+            ),
+            ListTile(
+              leading: const Icon(Icons.account_box_outlined),
+              title: const Text('Edit Account Details'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout_outlined),
+              title: const Text('Log out'),
+              onTap: () {},
+            ),
+          ],
+        ),
       );
 }
