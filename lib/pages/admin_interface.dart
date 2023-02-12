7df95847b74dbf1ca4647f611a8a3e71b1ab2719
Ali Alkhars <a-khars@hotmail.com>
2023-02-12 15:51:58 +0000

    remove old comment

--- lib/pages/admin_interface.dart
+++ lib/pages/admin_interface.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:seg_coursework_app/pages/admin_choice_boards.dart';
 
-// Create pages for all the menu items
-
 // This widget is the root of the admin interface
 class AdminInterface extends StatelessWidget {
   const AdminInterface({Key? key}) : super(key: key);
