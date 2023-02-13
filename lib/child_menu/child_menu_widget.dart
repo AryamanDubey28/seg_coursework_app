beaa38b6c2e2cfd809f05d26a2ff84c56c79496e
Brendan Rogan <K21041662@kcl.ac.uk>
2023-02-13 20:23:51 +0000

    Fix

--- lib/child_menu/child_menu_widget.dart
+++ lib/child_menu/child_menu_widget.dart
@@ -1,8 +1,6 @@
-import '../flutter_flow/flutter_flow_theme.dart';
-import '../flutter_flow/flutter_flow_util.dart';
+import 'flutter_flow/flutter_flow_theme.dart';
+import 'flutter_flow/flutter_flow_util.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
 import 'child_menu_model.dart';
 export 'child_menu_model.dart';
 
