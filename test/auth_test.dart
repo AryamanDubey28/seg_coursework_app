44e384bc2a0e6b7bef039332ee4744b273f92acc
Aryaman Dubey <aryaman.dubey@kcl.ac.uk>
2023-02-14 09:24:17 +0000

    make auth class to handle firebase auth

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -1,39 +1,49 @@
 import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:seg_coursework_app/Auth.dart';
 import 'package:seg_coursework_app/login.dart';
 import 'package:seg_coursework_app/main_page.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateNiceMocks([MockSpec<FirebaseAuth>()])
+import 'auth_test.mocks.dart';
 
 class MockUser extends Mock implements User {}
 
 final MockUser _mockUser = MockUser();
 
-class MockFirebaseAuth extends Mock implements FirebaseAuth {
-  @override
-  Stream<User?> authStateChanges() {
-    // TODO: implement authStateChanges
-    return Stream.fromIterable([
-      _mockUser,
-    ]);
-  }
-}
-
 void main() {
-  // final MockFirebaseAuth mockFirebaseAuth = MockFirebaseAuth();
-  // final MainPage mp = MainPage();
+  final mockFirebaseAuth = MockFirebaseAuth();
+  final Auth auth = Auth(auth: mockFirebaseAuth);
 
-  // setUp(() {});
+  setUp(() {});
 
-  // tearDown(() {});
+  tearDown(() {});
 
-  // test("test123", () async {
-  //   expectLater(mp.user, emitsInOrder([_mockUser]));
+  // test("emit occurs", () async {
+  //   expectLater(auth.user, emitsInOrder([_mockUser]));
   // });
 
-  // test("create user", () async {
-  //   when(mockFirebaseAuth.createUserWithEmailAndPassword(
-  //       email: "email", password: "password")).thenAnswer((realInvocation) => null);
-  // });
+  // when(mockFirebaseAuth.createUserWithEmailAndPassword(
+  //         email: "test@test.com", password: "Password123"))
+  //     .thenReturn(expected);
+
+  test("create account", () async {
+    print("creating dummy account");
+    expect(await auth.createAccount("test@test.com", "Password123"), "Success");
+  });
+
+  test("sign in", () async {
+    print("signing in account");
+    expect(await auth.signIn("test@test.com", "Password123"), "Success");
+  });
+
+  test("incorrect sign in", () async {
+    print("signing in account should fail");
+    expect(await auth.signIn("t@test.com", "Pass123"), "Unsuccessful");
+  });
 }
--- test/unit_test.dart
+++ test/unit_test.dart
@@ -6,23 +6,81 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:seg_coursework_app/Auth.dart';
 import 'package:seg_coursework_app/login.dart';
 
 void main() async {
-  final user = MockUser(
-    isAnonymous: false,
-    uid: 'MlKC55kgegXCDgVLQNUYd4h9f893',
-    email: 'aryaman@test.com',
-    displayName: 'Aryaman',
+  late Auth auth;
+  const _email = 'ilyas@yopmail.com';
+  const _uid = 'sampleUid';
+  const _displayName = 'ilyas';
+  const _password = 'Test@123';
+  final _mockUser = MockUser(
+    uid: _uid,
+    email: _email,
+    displayName: _displayName,
   );
-  final auth = MockFirebaseAuth(mockUser: user);
-  LogIn logIn = LogIn();
 
-  test("Testing log in is correct", () async {
-    //setup
-    when(auth.signInWithEmailAndPassword(email: "email", password: "password"));
-    UserCredential flag = await auth.signInWithEmailAndPassword(
-        email: "email", password: "password");
-    expect(flag.user!.email, "email");
+  setUp(() {
+    final _mockAuth = MockFirebaseAuth(mockUser: _mockUser);
+    auth = Auth(auth: _mockAuth);
   });
+
+  tearDown(() {});
+
+  test('signIn function test', () async {
+    final message = await auth.signIn(_email, _password);
+    print("message = $message");
+    expect(message, "Success");
+  });
+
+  test("incorrect log in test", () async {
+    final message = await auth.signIn("email", "password");
+    print("message = $message");
+    expect(message, "Unsuccessful");
+  });
+
+  // final user = MockUser(
+  //   isAnonymous: false,
+  //   uid: 'MlKC55kgegXCDgVLQNUYd4h9f893',
+  //   email: 'aryaman@test.com',
+  //   displayName: 'Aryaman',
+  // );
+  // final auth = MockFirebaseAuth(mockUser: user);
+
+  // test("Testing log in is correct", () async {
+  //   //setup
+  //   final user = MockUser(
+  //     isAnonymous: false,
+  //     uid: 'MlKC55kgegXCDgVLQNUYd4h9f893',
+  //     email: 'aryaman@test.com',
+  //     displayName: 'Aryaman',
+  //   );
+  //   final auth = MockFirebaseAuth(mockUser: user);
+
+  //   when(auth.signInWithEmailAndPassword(
+  //       email: "aryaman@test.com", password: "Password123"));
+  //   UserCredential flag = await auth.signInWithEmailAndPassword(
+  //       email: "aryaman@test.com", password: "Password123");
+  //   expect(flag.user!.email, "aryaman@test.com");
+  // });
+
+  // test("Testing incorrect log in", () async {
+  //   //setup
+  //   final user = MockUser(
+  //     isAnonymous: false,
+  //     uid: 'MlKC55kgegXCDgVLQNUYd4h9f893',
+  //     email: 'ary@test.com',
+  //     displayName: 'Aryaman',
+  //   );
+
+  //   final auth = MockFirebaseAuth(mockUser: user);
+  //   when(auth.signInWithEmailAndPassword(
+  //           email: "faillll@test.com", password: "Passssword123"))
+  //       .thenReturn(false);
+  //   UserCredential flag = await auth.signInWithEmailAndPassword(
+  //       email: "fail@test.com", password: "Password123");
+  //   print("flag = $flag  name = ${flag.user?.displayName}");
+  //   expect(flag.user!.email, "fail");
+  // });
 }
