adf2c4eb345c584efea856723e6ea373b3141858
Brendan Rogan <K21041662@kcl.ac.uk>
2023-02-22 16:37:13 +0000

    Test for button redirect

--- test/screens/child/child_menu_test.dart
+++ test/screens/child/child_menu_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:network_image_mock/network_image_mock.dart';
 import 'package:seg_coursework_app/pages/child_menu/customizable_column.dart';
 import 'package:seg_coursework_app/pages/child_menu/customizable_row.dart';
 import 'package:seg_coursework_app/widgets/category_image.dart';
@@ -18,5 +19,24 @@ void main() {
     expect(find.byType(CategoryImage), findsWidgets);
     expect(find.byType(CategoryTitle), findsWidgets);
     expect(find.byType(CategoryImageRow), findsWidgets);
+  });
+
+  testWidgets('Test tappable row directs to new screen', (tester) async {
+    mockNetworkImagesFor(() async {
+      await tester.pumpWidget(MaterialApp(
+        home: CustomizableRow(categoryTitle: "Title", imagePreviews: [
+          Image.asset("test/assets/test_image.png"),
+        ]),
+      ));
+
+      await tester.tap(find.byType(CustomizableRow));
+      await tester.pumpAndSettle();
+
+      expect(find.byKey(const ValueKey("boardMenu")), findsWidgets);
+      expect(find.byKey(const ValueKey("backButton")), findsWidgets);
+      expect(find.byKey(const ValueKey("categoryTitle")), findsWidgets);
+      expect(find.byKey(const ValueKey("categoryImage")), findsWidgets);
+      expect(find.byKey(const ValueKey("mainGridOfPictures")), findsWidgets);
+    });
   });
 }
