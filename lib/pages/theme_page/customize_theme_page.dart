960435d93f112a5c7fc66c8b9c841100b3660fbe
skellyhoe <y.s.hejelan@gmail.com>
2023-02-26 17:46:22 +0000

    Create a customize theme page

---
+++ lib/pages/theme_page/customize_theme_page.dart
@@ -0,0 +1,234 @@
+import 'package:flutter/material.dart';
+import 'package:flex_color_picker/flex_color_picker.dart';
+
+class CustomizeThemePage extends StatefulWidget {
+  const CustomizeThemePage({super.key});
+
+  @override
+  State<CustomizeThemePage> createState() => _CustomizeThemePageState();
+}
+
+class _CustomizeThemePageState extends State<CustomizeThemePage> {
+  Color? menuColor = Colors.teal[300];
+  Color? backgroundColor = Colors.teal[100];
+  Color? buttonsColor = Colors.teal[400];
+  Color? iconsAndTextsColor = Colors.white;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Customize Theme"),
+        leading: IconButton(
+          key: const Key("backButton"),
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+
+      body: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 300,
+                    height: 60,
+                    child: ListTile(
+                      title: Text("Menu color"),
+                      trailing: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          border:Border.all(color: Colors.black, width: 2),
+                          color: menuColor,
+                        ),
+                      ),
+                      onTap: () async {
+                        Color newMenuColor = await showColorPickerDialog(
+                          context, 
+                          menuColor!
+                        );
+                        if (newMenuColor != null) {
+                          setState(() {
+                            menuColor = newMenuColor;
+                          });
+                        }
+                      }
+                    ),
+                  ),
+            
+                  Container(
+                    width: 300,
+                    height: 60,
+                    child: ListTile(
+                      title: Text("Background color"),
+                      trailing: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          border:Border.all(color: Colors.black, width: 2),
+                          color: backgroundColor,
+                        ),
+                      ),
+                      onTap: () async {
+                        Color newBackgroundColor = await showColorPickerDialog(
+                          context, 
+                          backgroundColor!
+                        );
+                        if (newBackgroundColor != null) {
+                          setState(() {
+                            backgroundColor = newBackgroundColor;
+                          });
+                        }
+                      }
+                    ),
+                  ),
+            
+                  Container(
+                    width: 300,
+                    height: 60,
+                    child: ListTile(
+                      title: Text("Buttons color"),
+                      trailing: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          border:Border.all(color: Colors.black, width: 2),
+                          color: buttonsColor,
+                        ),
+                      ),
+                      onTap: () async {
+                        Color newButtonsColor = await showColorPickerDialog(
+                          context, 
+                          menuColor!
+                        );
+                        if (newButtonsColor != null) {
+                          setState(() {
+                            buttonsColor = newButtonsColor;
+                          });
+                        }
+                      }
+                    ),
+                  ),
+            
+                  Container(
+                    width: 300,
+                    height: 60,
+                    child: ListTile(
+                      title: Text("Icons and texts color"),
+                      trailing: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          border:Border.all(color: Colors.black, width: 2),
+                          color: iconsAndTextsColor,
+                        ),
+                      ),
+                      onTap: () {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text('Select a color'),
+                              actions: <Widget>[
+                                Container(
+                                  width: 50,
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                    border:Border.all(color: Colors.black, width: 2),
+                                    color: Colors.black,
+                                  ),
+                                  child: GestureDetector(
+                                    onTap: () {
+                                    // Set the color to black and close the dialog
+                                    Navigator.of(context).pop(Colors.black);
+                                    },
+                                  ),
+                                ),
+                                Container(
+                                  width: 50,
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                    border:Border.all(color: Colors.black, width: 2),
+                                    color: Colors.white,
+                                  ),
+                                  child: GestureDetector(
+                                    onTap: () {
+                                    // Set the color to black and close the dialog
+                                    Navigator.of(context).pop(Colors.white);
+                                    },
+                                  ),
+                                ),
+                              ]
+                            );
+                          },
+                        ).then((selectedColor) {
+                          if (selectedColor != null) {
+                            // Do something with the selected color
+                            // For example, update the state to rebuild the UI with the new color
+                            setState(() {
+                              iconsAndTextsColor = selectedColor;
+                            });
+                          }
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: backgroundColor,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    AppBar(
+                      leading: IconButton(
+                        icon: Icon(Icons.arrow_back, color: iconsAndTextsColor,), 
+                        onPressed: () {},
+                      ),
+                      backgroundColor: menuColor,
+                      elevation: 0,
+                      title: Text(
+                        'Menu',
+                        style: TextStyle(color: iconsAndTextsColor)
+                        // themeData.primaryTextTheme.headline6,
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    Column(
+                      children: [
+                        FloatingActionButton(
+                          elevation: 0,
+                          backgroundColor: buttonsColor,
+                          onPressed: () {},
+                          child: Icon(
+                            Icons.add,
+                            color: iconsAndTextsColor,
+                            // color: themeData.accentIconTheme.color,
+                          ),
+                        ),
+                        SizedBox(height: 16),
+                        Text(
+                          'Example Text',
+                          style: TextStyle(color: iconsAndTextsColor),
+                          // style: themeData.textTheme.headline6,
+                        ),
+                      ],
+                    ),
+                    SizedBox()
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/theme_page/theme_page.dart
+++ lib/pages/theme_page/theme_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:seg_coursework_app/widgets/theme_grid.dart';
 
+import 'customize_theme_page.dart';
+
 class ThemePage extends StatelessWidget {
   const ThemePage({super.key});
 
@@ -14,6 +16,19 @@ class ThemePage extends StatelessWidget {
           icon: const Icon(Icons.arrow_back),
           onPressed: () => Navigator.pop(context),
         ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CustomizeThemePage(),
+                ),
+              );
+            }, 
+            icon: Icon(Icons.edit)
+          )
+        ],
       ),
       body: Center(
         child: Container(
