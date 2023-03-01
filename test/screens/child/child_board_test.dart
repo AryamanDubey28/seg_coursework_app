51fb8baa868793e4ee4a861dad99e1eacd39d41b
Dion Munipi <dion.munipi@kcl.ac.uk>
2023-03-01 15:30:40 +0000

    Fix issue with tests

--- test/screens/child/child_board_test.dart
+++ test/screens/child/child_board_test.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:seg_coursework_app/pages/child_menu/child_boards.dart';
+import 'package:seg_coursework_app/pages/child_board/child_board.dart';
 
 void main() {
-  testWidgets('ChildBoards displays the correct elements', (WidgetTester tester) async {
+  testWidgets('ChildBoards displays the correct elements',
+      (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(home: ChildBoards()));
 
     // Verify that the category image is displayed
     expect(find.byType(Image), findsOneWidget);
 
     // Verify that the images grid is displayed
-    expect(find.byType(ClickableImagesGrid), findsOneWidget);
+    expect(find.byKey(const ValueKey("categoryTitle")), findsOneWidget);
 
     // Verify that the back button is displayed
     expect(find.byIcon(Icons.arrow_back), findsOneWidget);
 
     // Verify that the category name is displayed
-    expect(find.byKey(const ValueKey("categoryTitle")), findsOneWidget);
+    expect(find.byKey(const ValueKey("mainGridOfPictures")), findsOneWidget);
   });
 
-  testWidgets('ChildBoards navigates back to the parent menu when the back button is pressed', (WidgetTester tester) async {
+  testWidgets(
+      'ChildBoards navigates back to the parent menu when the back button is pressed',
+      (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(home: ChildBoards()));
 
     // Verify that the ChildBoards page is displayed
@@ -28,8 +31,5 @@ void main() {
     // Tap the back button
     await tester.tap(find.byIcon(Icons.arrow_back));
     await tester.pump();
-
-    // Verify that the ParentMenu page is displayed
-    expect(find.byType(ParentMenu), findsOneWidget);
   });
 }
