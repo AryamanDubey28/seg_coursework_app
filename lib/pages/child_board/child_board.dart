f1460fb3b4d2f729c308fe012c1333cd00e3f458
Dion Munipi <dion.munipi@kcl.ac.uk>
2023-03-01 15:12:25 +0000

    Improve tests and restructure code

--- lib/pages/child_board/child_board.dart
+++ lib/pages/child_board/child_board.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:seg_coursework_app/models/clickable_image.dart';
 import 'package:seg_coursework_app/pages/child_menu/customizable_column.dart';
 import 'package:seg_coursework_app/widgets/clickable_images_grid.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChildBoards extends StatefulWidget {
   const ChildBoards({Key? key}) : super(key: key);
+  // UNCOMMENT AFTER ADDING A QUEERY TO GET IMAGES, THIS SHOULD BE THE FUNCTION \/ DELETE THE CODE ABOVE
+  // const ChildBoards({Key? key, required String categoryTitle, required ClickableImage categoryImage, required List<ClickableImage> images}) : super(key: key);
 
   @override
   State<ChildBoards> createState() => _ChildBoards();
@@ -15,13 +16,12 @@ class ChildBoards extends StatefulWidget {
 class _ChildBoards extends State<ChildBoards> with TickerProviderStateMixin {
   // These are added to test while development
   // They will later be supplied from the database (TO BE DELETED)
+  // DELETE AFTER ADDING A QUEERY TO GET IMAGES
   final ClickableImage categoryImage = ClickableImage(
       name: "Toast",
       imageUrl:
           "https://www.simplyrecipes.com/thmb/20YogL0tqZKPaNft0xfsrldDj6k=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2010__01__cinnamon-toast-horiz-a-1800-5cb4bf76bb254da796a137885af8cb09.jpg",
       is_available: true);
-  // ClickableImage categoryImage;
-  // List<ClickableImage> images = [];
   final List<ClickableImage> images = [
     ClickableImage(
         name: "Toast",
@@ -64,11 +64,14 @@ class _ChildBoards extends State<ChildBoards> with TickerProviderStateMixin {
             "https://burgerandbeyond.co.uk/wp-content/uploads/2021/04/129119996_199991198289259_8789341653858239668_n-1.jpg",
         is_available: true),
   ];
+  // DELETE DOWN TO HERE
+  // UNCOMMENT AFTER ADDING A QUEERY TO GET IMAGES
+  // ClickableImage categoryImage;
+  // List<ClickableImage> images = [];
 
   @override
   void initState() {
     super.initState();
-    getImagesFromDatabase("items");
   }
 
   // Method used to build main body
@@ -175,27 +178,5 @@ class _ChildBoards extends State<ChildBoards> with TickerProviderStateMixin {
         )),
       ),
     );
-  }
-
-  // Adds all images from selected database category to the images list, also sets the category image and name
-  Future<void> getImagesFromDatabase(
-    String category,
-  ) async {
-    final CollectionReference<Map<String, dynamic>> documentReference =
-        FirebaseFirestore.instance.collection(category);
-
-    final QuerySnapshot<Map<String, dynamic>> documentSnapshots =
-        await documentReference.get();
-    // categoryImage = ClickableImage(
-    //     name: category,
-    //     imageUrl: documentSnapshots["illustration"],
-    //     is_available: true);
-
-    for (var doc in documentSnapshots.docs) {
-      images.add(ClickableImage(
-          name: doc["name"],
-          imageUrl: doc["illustration"],
-          is_available: doc["is_available"]));
-    }
   }
 }
--- test/screens/child/child_board_test.dart
+++ test/screens/child/child_board_test.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:network_image_mock/network_image_mock.dart';
-import 'package:seg_coursework_app/pages/child_board/child_board.dart';
+import 'package:seg_coursework_app/pages/child_menu/child_boards.dart';
 
 void main() {
-  testWidgets("Category has all components", (WidgetTester tester) async {
-    mockNetworkImagesFor(() async {
-      await tester.pumpWidget(const MaterialApp(home: ChildBoards()));
+  testWidgets('ChildBoards displays the correct elements', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: ChildBoards()));
 
-      expect(find.byKey(const ValueKey("boardMenu")), findsWidgets);
-      expect(find.byKey(const ValueKey("backButton")), findsWidgets);
-      expect(find.byKey(const ValueKey("categoryTitle")), findsWidgets);
-      expect(find.byKey(const ValueKey("categoryImage")), findsWidgets);
-      expect(find.byKey(const ValueKey("mainGridOfPictures")), findsWidgets);
-    });
+    // Verify that the category image is displayed
+    expect(find.byType(Image), findsOneWidget);
+
+    // Verify that the images grid is displayed
+    expect(find.byType(ClickableImagesGrid), findsOneWidget);
+
+    // Verify that the back button is displayed
+    expect(find.byIcon(Icons.arrow_back), findsOneWidget);
+
+    // Verify that the category name is displayed
+    expect(find.byKey(const ValueKey("categoryTitle")), findsOneWidget);
+  });
+
+  testWidgets('ChildBoards navigates back to the parent menu when the back button is pressed', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: ChildBoards()));
+
+    // Verify that the ChildBoards page is displayed
+    expect(find.byType(ChildBoards), findsOneWidget);
+
+    // Tap the back button
+    await tester.tap(find.byIcon(Icons.arrow_back));
+    await tester.pump();
+
+    // Verify that the ParentMenu page is displayed
+    expect(find.byType(ParentMenu), findsOneWidget);
   });
 }
