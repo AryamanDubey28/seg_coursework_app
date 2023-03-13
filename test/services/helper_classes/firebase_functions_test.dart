20c7b8e1c36b5c4857ae32841118e750dbd8fb99
Ali Alkhars <a-khars@hotmail.com>
2023-03-13 21:00:47 +0000

    Add unit tests for downloadUserCategories

--- test/services/helper_classes/firebase_functions_test.dart
+++ test/services/helper_classes/firebase_functions_test.dart
@@ -9,6 +9,7 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:firebase_storage_mocks/firebase_storage_mocks.dart';
 import 'package:seg_coursework_app/helpers/mock_firebase_authentication.dart';
+import 'package:seg_coursework_app/models/categories.dart';
 
 Future<void> main() async {
   late FirebaseFunctions firebaseFunctions;
@@ -18,6 +19,7 @@ Future<void> main() async {
   late MockUser mockUser;
 
   setUp(() {
+    TestWidgetsFlutterBinding.ensureInitialized();
     mockAuth = MockFirebaseAuthentication();
     mockFirestore = FakeFirebaseFirestore();
     mockStorage = MockFirebaseStorage();
@@ -34,7 +36,7 @@ Future<void> main() async {
   Future<DocumentSnapshot> _createCategory(
       {required String id, int rank = 0, required bool is_available}) async {
     mockFirestore.collection('categories').doc(id).set({
-      'name': "Drinks",
+      'title': "Drinks",
       'illustration': "drink.jpeg",
       'userId': mockUser.uid,
       'rank': rank,
@@ -1393,5 +1395,59 @@ Future<void> main() async {
     expect(item.get('rank'), 2);
     expect(item1.get('rank'), 0);
     expect(item2.get('rank'), 1);
+  });
+
+  test(
+      "downloadUserCategories converts choice board's data correctly into Categories datatype",
+      () async {
+    const String name = "Water";
+    const String imageUrl = "Nova-water.jpeg";
+    const String categoryId1 = "00xx";
+    const String categoryId2 = "11yy";
+
+    await _createCategory(id: categoryId1, is_available: true);
+    await _createCategory(id: categoryId2, is_available: false, rank: 1);
+
+    String newItemId =
+        await firebaseFunctions.createItem(name: name, imageUrl: imageUrl);
+    String newItemId1 =
+        await firebaseFunctions.createItem(name: name, imageUrl: imageUrl);
+    String newItemId2 =
+        await firebaseFunctions.createItem(name: name, imageUrl: imageUrl);
+
+    await firebaseFunctions.createCategoryItem(
+        name: name,
+        imageUrl: imageUrl,
+        categoryId: categoryId1,
+        itemId: newItemId);
+    await firebaseFunctions.createCategoryItem(
+        name: name,
+        imageUrl: imageUrl,
+        categoryId: categoryId2,
+        itemId: newItemId1);
+    await firebaseFunctions.createCategoryItem(
+        name: name,
+        imageUrl: imageUrl,
+        categoryId: categoryId2,
+        itemId: newItemId2);
+
+    Categories userCategories =
+        await firebaseFunctions.downloadUserCategories();
+
+    expect(userCategories.getList().length, 2);
+    expect(userCategories.getList()[0].id, categoryId1);
+    expect(userCategories.getList()[1].id, categoryId2);
+    expect(userCategories.getList()[0].availability, true);
+    expect(userCategories.getList()[1].availability, false);
+    expect(userCategories.getList()[0].children.length, 1);
+    expect(userCategories.getList()[1].children.length, 2);
+  });
+
+  test(
+      "downloadUserCategories returns an empty Categories datatype if user has no choice boards data",
+      () async {
+    Categories userCategories =
+        await firebaseFunctions.downloadUserCategories();
+    expect(userCategories.getList().length, 0);
   });
 }
