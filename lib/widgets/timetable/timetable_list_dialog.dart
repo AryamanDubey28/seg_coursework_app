6981ac37e49a6d2cfab271028e2a47401b9270cb
skellyhoe <y.s.hejelan@gmail.com>
2023-03-20 21:17:21 +0000

    Add title to timetable list dialog

--- lib/widgets/timetable/timetable_list_dialog.dart
+++ lib/widgets/timetable/timetable_list_dialog.dart
@@ -24,76 +24,83 @@ class _TimetableListDialogState extends State<TimetableListDialog> {
 
     return LayoutBuilder(
       builder: (context, constraints) {
-        return SizedBox(
-          width: (constraints.maxWidth * (5/6) + (constraints.maxWidth/35*4)),
-          height: constraints.maxWidth/(widget.timetable.length()+1),
-          child: Center(
-            child: ListView.separated(
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              itemCount: widget.timetable.length(),
-              itemBuilder: (context, index) {
-                return InkWell(
-                  onTap: () {
-                    setState(() {
-                      if(crossedOutIndices.contains(index))
-                      {
-                        crossedOutIndices.remove(index);
-                      }
-                      else
-                      {
-                        crossedOutIndices.add(index);
-                      }
-                    });
-                  },
-                  child: Stack(
-                    children: <Widget>[
-                      ImageSquare(
-                        //This width is set to make the image change with how big the screen is and how many images
-                        width: constraints.maxWidth/(widget.timetable.length()+1),
-                        height: constraints.maxHeight,
-                        key: Key('timetableDialogImage$index'),
-                        image: widget.timetable[index],
-                      ),
-                      if (crossedOutIndices.contains(index))
-                        //The cross out design
-                        Positioned.fill(
-                          child: Container(
-                            key: Key("cross$index"),
-                            decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            color: Colors.red.withOpacity(0.5),
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(widget.timetable.title, style: const TextStyle(fontSize: 24),),
+            const SizedBox(height: 50,),
+            SizedBox(
+              width: (constraints.maxWidth * (5/6) + (constraints.maxWidth/35*4)),
+              height: constraints.maxWidth/(widget.timetable.length()+1),
+              child: Center(
+                child: ListView.separated(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: widget.timetable.length(),
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {
+                        setState(() {
+                          if(crossedOutIndices.contains(index))
+                          {
+                            crossedOutIndices.remove(index);
+                          }
+                          else
+                          {
+                            crossedOutIndices.add(index);
+                          }
+                        });
+                      },
+                      child: Stack(
+                        children: <Widget>[
+                          ImageSquare(
+                            //This width is set to make the image change with how big the screen is and how many images
+                            width: constraints.maxWidth/(widget.timetable.length()+1),
+                            height: constraints.maxHeight,
+                            key: Key('timetableDialogImage$index'),
+                            image: widget.timetable[index],
                           ),
-                          clipBehavior: Clip.hardEdge,
-                            child: Center(
-                              child: DecoratedIcon(
-                                decoration: const IconDecoration(border: IconBorder(width: 5)),
-                                icon: Icon(
-                                  Icons.clear,
-                                  color: Colors.red,
-                                  size: (constraints.maxWidth) / 2 / widget.timetable.length(),
-                                )
+                          if (crossedOutIndices.contains(index))
+                            //The cross out design
+                            Positioned.fill(
+                              child: Container(
+                                key: Key("cross$index"),
+                                decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Colors.red.withOpacity(0.5),
+                              ),
+                              clipBehavior: Clip.hardEdge,
+                                child: Center(
+                                  child: DecoratedIcon(
+                                    decoration: const IconDecoration(border: IconBorder(width: 5)),
+                                    icon: Icon(
+                                      Icons.clear,
+                                      color: Colors.red,
+                                      size: (constraints.maxWidth) / 2 / widget.timetable.length(),
+                                    )
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
-                        ),
-                    ],
-                  ),
-                );
-              },
-              //This is so there's no arrow after the last picture.
-              separatorBuilder: (context, index) {
-                bool isWhite = themeNotifier.getTheme().iconTheme.color == Colors.white;
-                return DecoratedIcon(
-                  decoration: IconDecoration(border: IconBorder(width: 5, color: isWhite ? Colors.black : Colors.white)),
-                  icon: Icon(
-                    Icons.arrow_right,
-                    size: constraints.maxWidth/35
-                  )
-                );
-              },
+                        ],
+                      ),
+                    );
+                  },
+                  //This is so there's no arrow after the last picture.
+                  separatorBuilder: (context, index) {
+                    bool isWhite = themeNotifier.getTheme().iconTheme.color == Colors.white;
+                    return DecoratedIcon(
+                      decoration: IconDecoration(border: IconBorder(width: 5, color: isWhite ? Colors.black : Colors.white)),
+                      icon: Icon(
+                        Icons.arrow_right,
+                        size: constraints.maxWidth/35
+                      )
+                    );
+                  },
+                ),
+              ),
             ),
-          ),
+          ],
         );
       }
     );
