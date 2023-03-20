8bfd407379b73a63f0968f3325dbc75f71e8a1e1
skellyhoe <y.s.hejelan@gmail.com>
2023-03-20 14:40:01 +0000

    Delete comment.

--- lib/pages/admin/add_existing_item.dart
+++ lib/pages/admin/add_existing_item.dart
@@ -47,7 +47,6 @@ class _AddExistingItem extends State<AddExistingItem> {
           title: const Text('Add an Existing Item'),
           leading: IconButton(
             key: const Key("createItemButton"),
-            // iconSize: 40,
             icon: const Icon(Icons.arrow_back),
             onPressed: () {
               Navigator.of(context).pop();
